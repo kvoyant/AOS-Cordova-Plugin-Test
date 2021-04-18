e80aecbc38eb5edd8c992a248f5daa55fa394a99
김영호 <kvoyant@gmail.com>
2021-04-18 10:35:37 +0900

    qr 테스트 성공 - 클릭이벤트로 호출시 성공

--- app/src/main/assets/www/js/index.js
+++ app/src/main/assets/www/js/index.js
@@ -27,3 +27,77 @@ function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
 }
+
+function init() {
+    document.querySelector("#startScan").addEventListener("touchend", startScan, false);
+    resultDiv = document.querySelector("#results");
+}
+
+function searchQr() {
+    console.log('searchQr()~!');
+
+    cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : true, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+    );
+}
+
+
+
+
+//function startScan() {
+//    cordova.plugins.barcodeScanner.scan(
+//        function re (result) {
+//            //바코드가 읽힌 결과 확인
+//            /*
+//            var s = "Result: " + result.text + "<br/>" +
+//            "Format: " + result.format + "<br/>" +
+//            "Cancelled: " + result.cancelled;
+//            */
+//            //바코드 읽은 결과를 실제 browser에 띄우기
+//             var ref = window.open(result.text, '_self', 'location=no');
+//             ref.addEventListener('loadstart', function(event) {
+//                 var urlSuccessPage = result.txt;
+//                 if (event.url == urlSuccessPage) {
+//                 ref.close();
+//                 }
+//             });
+//            alert('test');
+//        },
+//        function (error) {
+//            alert("Scanning failed: " + error);
+//        }
+//    );
+//}
+
+// cordova.plugins.barcodeScanner.scan(
+//      function (result) {
+//          alert("A barcode has been scanned \n" +
+//                "Result: " + result.text + "\n" +
+//                "Format: " + result.format + "\n" +
+//                "Cancelled: " + result.cancelled);
+//      },
+//      function (error) {
+//          alert("Scanning failed: " + error);
+//      }
+// );
