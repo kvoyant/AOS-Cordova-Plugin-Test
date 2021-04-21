1eca5537265f778e697f7b9a8962b4fc71266afe
김영호 <kvoyant@gmail.com>
2021-04-21 10:32:31 +0900

    Geolocation plugin 추가 - cordova plugin add cordova-plugin-geolocation - 아래 바코드 스캐너 : cordova plugin add cordova-plugin-qr-barcode-scanner

--- app/src/main/assets/www/cordova_plugins.js
+++ app/src/main/assets/www/cordova_plugins.js
@@ -7,10 +7,25 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "cordova.plugins.barcodeScanner"
       ]
+    },
+    {
+      "id": "cordova-plugin-geolocation.geolocation",
+      "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+      "pluginId": "cordova-plugin-geolocation",
+      "clobbers": [
+        "navigator.geolocation"
+      ]
+    },
+    {
+      "id": "cordova-plugin-geolocation.PositionError",
+      "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+      "pluginId": "cordova-plugin-geolocation",
+      "runs": true
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-qr-barcode-scanner": "8.0.3"
+    "cordova-plugin-qr-barcode-scanner": "8.0.3",
+    "cordova-plugin-geolocation": "4.1.0"
   };
 });
